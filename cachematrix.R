4779ea774c76c9256914291813550322f2eb4f8c
Irving Méreau <irving@MacBook-Pro.local>
2014-07-26 15:16:11 +0200

    my version of the cache matrix code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below code will use caching to cumpute the invert of a matrix.
+# It will get the value from cache if it has been computed already, else compute it and cache it
 
-## Write a short comment describing this function
+# This function creates a special matrix containing a function to:
+#     - set the value of the matrix
+#     - get the value of the matrix
+#     - set the invert of the matrix
+#     - get the invert of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+      m<-NULL
+      set<-function(y){
+            x<<-y
+            m<<-NULL
+      }
+      get<-function() x
+      setmatrix<-function(solve) m<<- solve
+      getmatrix<-function() m
+      list(set=set, get=get,
+           setmatrix=setmatrix,
+           getmatrix=getmatrix)
+      
 }
 
 
-## Write a short comment describing this function
+# The following function calculates the invert of the special "matrixr" created with the above function. 
+# However, it first checks to see if the invert has already been calculated. 
+#     - If so, it gets the mean from the cache and skips the computation. 
+#     - Otherwise, it calculates the invert and sets the value of the inverted matrix in the cache via the setatrix function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      
+      m<-x$getmatrix()
+      if(!is.null(m)){
+            message("getting cached data")
+            return(m)
+      }
+      matrix<-x$get()
+      m<-solve(matrix, ...)
+      x$setmatrix(m)
+      m
 }
